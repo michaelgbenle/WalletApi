fcc205125d957bd1d0cb20d3eb363e78824cf82d
Michael <michaelgbenle@gmail.com>
2022-07-04 15:40:40 +0100

    wallet api

--- database/interface.go
+++ database/interface.go
@@ -1,1 +1,39 @@
 package database
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type DB interface {
+}
+
+type DbParameters struct {
+	Host     string
+	User     string
+	Password string
+	DbName   string
+	Port     string
+}
+
+func InitializeDbParameters() DbParameters {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	return DbParameters{
+		Host:     host,
+		User:     user,
+		Password: password,
+		DbName:   dbName,
+		Port:     port,
+	}
+}
+
--- server/server.go
+++ server/server.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"github.com/michaelgbenle/WalletApi/database"
+	"github.com/michaelgbenle/WalletApi/handler"
+	"log"
+)
+
+func Start() error {
+	values := database.InitializeDbParameters()
+	var PDB = new(database.PostgresDb)
+	h := &handler.Handler{DB: PDB}
+
+	err := PDB.SetupDb(values.Host, values.User, values.Password, values.DbName, values.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	routes, port := router.SetupRouter(h)
+	err = routes.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
