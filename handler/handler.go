b6f6ede4f92c6dea744959721b2ca5b8197cc761
Michael <michaelgbenle@gmail.com>
2022-07-04 15:49:10 +0100

    wallet api

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,36 @@
 package handler
 
-import "github.com/michaelgbenle/WalletApi/database"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/michaelgbenle/WalletApi/database"
+	"net/http"
+)
 
 type Handler struct {
 	DB database.DB
+}
+
+func (h *Handler) GetWallets(c *gin.Context) {
+	id := c.Query("id")
+	customer, err := h.DB.Wallet(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "user not found",
+		})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": customer,
+	})
+}
+func (h *Handler) GetTransaction(c *gin.Context) {
+
+}
+
+func (h *Handler) CreditWallet(c *gin.Context) {
+	id := c.Query("id")
+
+}
+func (h *Handler) DebitWallet(c *gin.Context) {
+
 }
