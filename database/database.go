1c5d5883de753cc711b415c0250d30680dc3ec55
Michael <michaelgbenle@gmail.com>
2022-07-04 15:44:21 +0100

    wallet api

--- database/database.go
+++ database/database.go
@@ -1,7 +1,27 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
 
 type PostgresDb struct {
 	DB *gorm.DB
+}
+
+func (pdb *PostgresDb) SetupDb(host, user, password, dbName, port string) error {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbName, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	pdb.DB = db
+
+	dberr := pdb.DB.AutoMigrate(&models.Book{})
+	if dberr != nil {
+		log.Fatal(dberr)
+	}
+	return nil
 }
