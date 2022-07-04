e90689702da197e18b5e6f8708c0edabaebcb7ef
Michael <michaelgbenle@gmail.com>
2022-07-04 17:43:59 +0100

    wallet api

--- database/interface.go
+++ database/interface.go
@@ -10,7 +10,7 @@ import (
 type DB interface {
 	Getcustomer(id string) (*models.Customer, error)
 	Addcustomer(customer *models.Customer) error
-	Creditwallet(money *models.Money) error
+	Creditwallet(money *models.Money) (models.Transaction, error)
 	Debitwallet(money models.Money) error
 }
 
