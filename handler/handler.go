ab09ad1609b615f3f9d8d060a27e72f54cf7edc3
Michael <michaelgbenle@gmail.com>
2022-07-04 17:28:44 +0100

    wallet api

--- handler/handler.go
+++ handler/handler.go
@@ -44,7 +44,7 @@ func (h *Handler) DebitWallet(c *gin.Context) {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "unable to bind json"})
 		return
 	}
-	if CreateErr := h.DB.Creditwallet(credit).Error; CreateErr != nil {
+	if CreateErr := h.DB.Deditwallet(debit).Error; CreateErr != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "unable to credit wallet"})
 		return
 	}
