e1cdddcc3aca0401a1554fbf70e934590966937a
Michael <michaelgbenle@gmail.com>
2022-07-05 10:30:56 +0100

    wallet api

--- handler/test/handler_test.go
+++ handler/test/handler_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"github.com/golang/mock/gomock"
 	mockdatabase "github.com/michaelgbenle/WalletApi/database/mocks"
+	"github.com/michaelgbenle/WalletApi/handler"
 	"testing"
 )
 
@@ -10,5 +11,6 @@ func TestGetCustomer(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	mockDB := mockdatabase.NewMockDB(ctrl)
+	h := handler.Handler{DB: mockDB}
 
 }
