257a678415c5f641992cbb67cf86fcf08c2796e0
Michael <michaelgbenle@gmail.com>
2022-07-05 09:01:42 +0100

    wallet api

--- database/database.go
+++ database/database.go
@@ -75,6 +75,7 @@ func (pdb *PostgresDb) InsufficientFunds(money *models.Money) error {
 	}
 	return nil
 }
+func (pdb *PostgresDb) CreateTransaction(money *models.Money) {}
 
 func (pdb *PostgresDb) Debitwallet(money *models.Money) (*models.Transaction, error) {
 	accountNos, amount := money.AccountNos, money.Amount
